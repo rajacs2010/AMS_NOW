3a9f553cebcf1b0c0c92c602d36bc239270a8deb
raja <rajacs34@gmail.com>
2014-01-02 13:18:05 +0530

    commited by raja
    raja0212013

--- ajax_viewvendor.php
+++ ajax_viewvendor.php
@@ -1,178 +1,131 @@
 <?PHP
 require_once("./include/membersite_config.php");
-
+require_once ("./include/ajax_pagination.php");
+EXTRACT($_REQUEST);
+$fgmembersite->DBLogin();
+if(!$fgmembersite->CheckLogin()) {
+    $fgmembersite->RedirectToURL("index.php");
+    exit;
+}
+require_once("./include/membersite_config.php");
 if(!$fgmembersite->CheckLogin())
 {
     $fgmembersite->RedirectToURL("index.php");
     exit;
 }
+if($_REQUEST['searchname']!='') {
+	$var = @$_REQUEST['searchname'] ;
+	$trimmed = trim($var);	
+	$qry="SELECT * FROM vendor where name like '%".$trimmed."%'";
+} else { 
+	$qry="SELECT * FROM vendor"; 
+}
+$results=mysql_query($qry);
+$num_rows= mysql_num_rows($results);			
+$params			=	$searchname."&".$sortorder."&".$ordercol;
 
+/********************************pagination start***********************************/
+$strPage = $_REQUEST[page];
+//$params = $_REQUEST[params];
+
+//if($_REQUEST[mode]=="Listing"){
+//$Num_Rows = mysql_num_rows ($res_search);
+
+########### pagins
+
+$Per_Page = 5;   // Records Per Page
+
+$Page = $strPage;
+if(!$strPage)
+{
+	$Page=1;
+}
+
+$Prev_Page = $Page-1;
+$Next_Page = $Page+1;
+
+$Page_Start = (($Per_Page*$Page)-$Per_Page);
+if($num_rows<=$Per_Page)
+{
+$Num_Pages =1;
+}
+else if(($num_rows % $Per_Page)==0)
+{
+$Num_Pages =($num_rows/$Per_Page) ;
+}
+else
+{
+$Num_Pages =($num_rows/$Per_Page)+1;
+$Num_Pages = (int)$Num_Pages;
+}
+if($sortorder == "")
+{
+	$orderby	=	"ORDER BY id DESC";
+} else {
+	$orderby	=	"ORDER BY $ordercol $sortorder";
+}
+$qry.=" $orderby LIMIT $Page_Start , $Per_Page";  //need to uncomment
+//exit;
+$results_dsr = mysql_query($qry) or die(mysql_error());
+/********************************pagination***********************************/
 ?>
-<link media="screen" rel="stylesheet" href="style/fixed_table_header/960.css">
-        <link media="screen" rel="stylesheet" href="style/fixed_table_header/defaultTheme.css">
-        <link media="screen" rel="stylesheet" href="style/fixed_table_header/myTheme.css">
-        <script src="scripts/fixed_table_header/jquery.min.js"></script>
-        <script src="scripts/fixed_table_header/jquery.fixedheadertable.min.js"></script>
-        <script src="scripts/fixed_table_header/demo.js"></script>
-		<script>
-		function show_confirm()
-{
-var r=confirm("Are you sure want to delete");
-if (r==true)
-  {
-  return true;
-  }
-else
-  {
-  return false;
-  }
-}
-</script>
-<?php
-	$fgmembersite->DBLogin();
-//PAGE NUMBER, RESULTS PER PAGE, AND OFFSET OF THE RESULTS
-if($_GET["page"]){
-    $pagenum = $_GET["page"];
-} else {
-    $pagenum = 1;
-}
-
-$rowsperpage = 7; //MAXIMUM RESULTS PER PAGE
-$offset = ($pagenum - 1) * $rowsperpage; //WHERE THE RESULTS START FROM
-
-//FOR RESULTS OF THE PAGE
-if($_GET["searchvalue"]){
-$search=$_GET["searchvalue"];
-    $q = mysql_query("SELECT * FROM vendor where vendor_name like '%$search%' ORDER BY id LIMIT $offset, $rowsperpage");
-} else {
-    $q = mysql_query("SELECT * FROM vendor ORDER BY id LIMIT $offset, $rowsperpage");
-}
-
-$page_nums = mysql_num_rows($q); //NUMBER OF RESULTS FOR THE PAGE
-if ($page_nums!=0)
-{
-if($_GET["searchvalue"]){
-$search=$_GET["searchvalue"];
-	 $total_q = mysql_query("SELECT * FROM vendor where vendor_name like '%$search%' "); //FOR THE ALL RESULTS
-} else {
-    $total_q = mysql_query("SELECT * FROM vendor"); //FOR THE ALL RESULTS
-}
-
-$total_nums = mysql_num_rows($total_q); //TOTAL NUMBER OF RESULTS
-$total_pages = ceil($total_nums/$rowsperpage); //NUMBER OF PAGES
-//<table class="myTable01">
-//IF PAGE NUMBER IS WITHIN THE FIRST AND LAST PAGES
-if($pagenum>=1&&$pagenum<=$total_pages)
-{
-
-echo '
-<table class="owntable">
+<div class="con2">
+<table width="100%">
 <thead>
 <tr>
-<th>vendor Code</th>
-<th>Name</th>
-<th>Address line 1</th>
-<th>Contact number</th>
-<th>Edit</th>
-<th>Delete</th>
-
-</tr>
-</thead>
-<tbody>';
-
-    while($r=mysql_fetch_array($q))
-    {
 	
-	
-        $vendor_code = $r["vendor_code"];
-		$emp_id = $r["id"];
-		$vendor_name=$r["vendor_name"];
-		$address1=$r["address1"];
-		$contact_number=$r["contact_number"];
-        echo '<tr><td>'.$vendor_code.'</td><td>'.$vendor_name.'</td><td>'.$address1.'</td><td>'.$contact_number.'</td><td><a href=edit_vendor.php?id='.$emp_id.'><img src="images/user_edit.png"/></a></td>
-		<td><a href=view_vendor.php?delete_id='.$emp_id.'&delete=1><img src="images/delete.png" onclick="return show_confirm()"/></a></td></tr>';
-	
-    }
-    echo '</tbody></table>';
-    echo '<br>';
-    
-    //IF ANY RESULTS
-    if($total_nums)
-    {
-        $range = 4; //NUMBER OF PAGES TO BE SHOWN BEFORE AND AFTER THE CURRENT PAGE NUMBER
-        
-        //FIRST, PREVIOUS, NEXT, AND LAST LINKS
-        if($pagenum>1)
-        {
-            $page = $pagenum - 1;
-            $first = '<a class="page" id="1" >First</a> ';
-            $prev = '<a class="page" id="'.$page.'"><<</a> ';
-        }
-        if($pagenum<$total_pages)
-        {
-            $page = $pagenum + 1;
-            $next = '<a class="page" id="'.$page.'">>></a> ';
-            $last = '<a class="page" id="'.$total_pages.'">Last</a> ';
-        }
-        
-        //PAGINATION
-        for($page=($pagenum-$range); $page<=($pagenum+$range); $page++)
-        {
-            if($page>=1&&$page<=$total_pages)
-            {
-                if($page==$pagenum)
-                {
-                    $nav .= '<span class="pagenum">'.$page.'</span> ';
-                }
-                else
-                {
-                    $nav .= '<a class="page" id="'.$page.'">'.$page.'</a> ';
-                }
-            }
-        }
-    }
-    
-    //DISPAYS IN HTML
-	if ($total_nums>$rowsperpage)
-	{
-	 echo $first . $prev . $nav . $next . $last;
+	<?php //echo $sortorderby;
+	if($sortorder == 'ASC') {
+		$sortorderby = 'DESC';
+	} elseif($sortorder == 'DESC') {
+		$sortorderby = 'ASC';
+	} else {
+		$sortorderby = 'ASC';
 	}
-	
-} 
-else {
-    //OTHERWISE...
-    header("Location: ajax_viewvendor.php"); //WILL REDIRECT TO THE FIRST PAGE OF RESULTS
-}
-}
-else
-{
-$q_initial = mysql_query("SELECT * FROM vendor");
-$page_nums_initial = mysql_num_rows($q_initial);
-if ($page_nums_initial !=0)
-{
-echo '
-<table class="owntable">
-<thead>
-<tr>
-<th>vendor Code</th>
-<th>Name</th>
-<th>Address line 1</th>
-<th>Contact number</th>
-<th>Edit</th>
-<th>Delete</th>
+	$paramsval	=	$searchname."&".$sortorderby."&vendor_code"; ?>
+				<th nowrap="nowrap" class="rounded" onClick="colviewajax('<?php echo $Page; ?>','<?php echo $paramsval; ?>');">Vendor Code<img src="images/sort.png" width="13" height="13" /></th>
+				<th nowrap="nowrap" >Name</th>
+				<th nowrap="nowrap" >Contact Number</th>
+				<th nowrap="nowrap" >Edit</th>
+				<th nowrap="nowrap" >Delete</th>
 </tr>
 </thead>
 <tbody>
-<tr>
-<td align="right" colspan="6">
-<font style="font-size: 16px;color:red;padding-right:230px;">No Records Found</font>
-</td>
-
-
+<?php
+if(!empty($num_rows)){
+	$slno	=	($Page-1)*$Per_Page + 1;
+$c=0;$cc=1;
+while($fetch = mysql_fetch_array($results_dsr)) {
+if($c % 2 == 0){ $cls =""; } else{ $cls =" class='odd'"; }
+$id= $fetch['id'];
+?>
+<tr>				
+				<td><?php echo $fetch['vendor_code'];?></td>
+				<td><?php echo $fgmembersite->upperstate($fetch['name']); ?></td>
+				<td><?php echo $fetch['contact_number'];?></td>
+				<td >
+				<a href="edit_vendor.php?id=<?php echo $fetch['id'];?>"><img src="images/user_edit.png" alt="" title="" width="11" height="11"/></a>
+				</td>
+				<td>
+				<a href="view_vendor.php?delete_id=<?php echo $fetch['id'];?>&delete=1"><img src="images/trash.png" alt="" title="" width="11" height="11" onclick="return show_confirm('<?php echo $fetch['vendor_code']; ?>');"/></a>
+				</td>
 </tr>
+<?php $c++; $cc++; $slno++; }		 
+}else{  echo "<tr><td align='center' colspan='5'><b>No records found</b></td></tr>";}  ?>
 </tbody>
 </table>
-';
-}
-}
-?>
+ </div>   
+ <div class="paginationfile" align="center">
+ <table>
+ <tr>
+ <th class="pagination" scope="col">          
+<?php 
+if(!empty($num_rows)){
+	rendering_pagination_common($Num_Pages,$Page,$Prev_Page,$Next_Page,$params,'colviewajax');   //need to uncomment
+} else { 
+	echo "&nbsp;"; 
+} ?>      
+</th>
+</tr>
+</table>
+</div>
