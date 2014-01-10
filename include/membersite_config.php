d84006ab84022d9f66f0096483b039525f33492e
raja <rajacs34@gmail.com>
2014-01-10 17:17:48 +0530

    cxczx
    zxcxzc

--- include/membersite_config.php
+++ include/membersite_config.php
@@ -13,12 +13,12 @@ $fgmembersite->SetAdminEmail('raja.nuvento@gmail.com');
 //hostname, user name, password, database name and table name
 //note that the script will create the table (for example, fgusers in this case)
 //by itself on submitting register.php for the first time
-$fgmembersite->InitDB(/*hostname*/'localhost',
+$fgmembersite->InitDB(/*hostname*/'192.168.1.25',
                       /*username*/'root',
                       /*password*/'hari',
                       /*database name*/'fmcl',
                       /*table name*/'users');
-					$mysql_hostname = "localhost";
+					$mysql_hostname = "192.168.1.25";
 					$mysql_user = "root";
 					$mysql_password = "hari";
 					$mysql_database = "fmcl_host";
